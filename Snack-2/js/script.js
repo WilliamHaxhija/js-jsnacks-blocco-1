39748d30435ae5b268d03fe760c328e6b7a9304e
WilliamHaxhija <williamhaxhija@gmail.com>
2024-03-11 16:59:55 +0100

    feat: add snack-2

--- Snack-2/js/script.js
+++ Snack-2/js/script.js
@@ -1,7 +1,11 @@
-//Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
-
 //Creo una variabile sum e le assegno valore 0
+let sum = 0;
 //Creo un ciclo di 5 iterazioni
-  // - Creo un prompt all'interno del ciclo all'interno del quale l'utente inserisce un numero
-  //Riassegno all'interno del ciclo la variabile sum uguale a se stessa + il numero inserito dall'utente
+for ( let i = 0; i < 5; i++) {
+    // - Creo un prompt all'interno del ciclo all'interno del quale l'utente inserisce un numero
+    const userNumber = parseInt(prompt('Scrivi un numero'));
+    //Riassegno all'interno del ciclo la variabile sum uguale a se stessa + il numero inserito dall'utente
+    sum += userNumber;
+}
 //Stampo fuori dal ciclo la somma di tutti i numeri in console
+console.log(sum);
