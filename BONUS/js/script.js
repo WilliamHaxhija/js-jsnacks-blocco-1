9a224d018bd0768e4d5768784c8ad26514dc95d6
WilliamHaxhija <williamhaxhija@gmail.com>
2024-03-11 17:47:46 +0100

    feat: add BONUS

--- BONUS/js/script.js
+++ BONUS/js/script.js
@@ -1,1 +1,11 @@
 //Dato un array contenente 10 numeri, stampare in console il numero più grande.
+
+const number1To10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let biggerNumber = number1To10[0];
+for (let i = 1; i < number1To10.length; i++) {
+    if (number1To10[i] > biggerNumber) {
+        biggerNumber = number1To10[i];
+    }
+}
+console.log(number1To10);
+console.log(biggerNumber + " è il numero più grande dell'array");
