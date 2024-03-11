a4f05b1712503e6ff5b4fd582b984398831c3ebf
WilliamHaxhija <williamhaxhija@gmail.com>
2024-03-11 16:55:24 +0100

    feat: add unpacked problem

--- Snack-2/js/script.js
+++ Snack-2/js/script.js
@@ -1,1 +1,7 @@
 //Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti.
+
+//Creo una variabile sum e le assegno valore 0
+//Creo un ciclo di 5 iterazioni
+  // - Creo un prompt all'interno del ciclo all'interno del quale l'utente inserisce un numero
+  //Riassegno all'interno del ciclo la variabile sum uguale a se stessa + il numero inserito dall'utente
+//Stampo fuori dal ciclo la somma di tutti i numeri in console
