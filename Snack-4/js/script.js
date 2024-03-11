711c1e5ee07d6b24b4a2001efe59d63334d671a7
WilliamHaxhija <williamhaxhija@gmail.com>
2024-03-11 17:15:44 +0100

    Init snack-4

---
+++ Snack-4/js/script.js
@@ -0,0 +1,6 @@
+//Calcola la somma e la media dei primi 10 numeri.
+
+//Creo una variabile
+//Creo ciclo for con 10 iterazoni
+  //Dentro il ciclo stampo un numero per ogni iterazone e lo assegno alla variabile sommandolo la valore precedente della variabile
+//Fuori dal ciclo divido per 10 la variabile e trovo la media
