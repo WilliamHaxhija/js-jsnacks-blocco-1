f99083263ce23254f540f775ba08d8f3bb25b168
WilliamHaxhija <williamhaxhija@gmail.com>
2024-03-11 16:41:01 +0100

    feat: add snack-1 unpacked problem

--- Snack-1/js/script.js
+++ Snack-1/js/script.js
@@ -1,1 +1,10 @@
 //Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare duplicati (inserire il numero nell'array solo se non è già presente).
+
+//Creo un array vuoto
+
+//Chiedo all'utente 5 numeri
+  // - Creo un prompt all'interno di un ciclo for che esegue 5 iterazioni
+
+//Inserisco i numeri in un array, senza creare dupilcati
+  //Con la unzione .includes controllo se il numero è già presente nell'array
+    // - Se non c'è, lo inserisco nell'array con la funzione .push
