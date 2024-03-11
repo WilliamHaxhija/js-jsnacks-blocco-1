7f1949a18aa20c0f8e60fe9e72d56b2d384afedb
WilliamHaxhija <williamhaxhija@gmail.com>
2024-03-11 17:05:23 +0100

    feat: add unpacked problem + fix snack-2 HTML title

---
+++ Snack-3/js/script.js
@@ -0,0 +1,6 @@
+//Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
+
+//Creo un array vuoto
+//Creo un ciclo con 6 iterazioni
+  //Creo un prompt all'interno del ciclo e chiedo un numero all'utente
+  //Creo una condizione per la quale se il numero è dispari viene inserito all'interno dell'array
